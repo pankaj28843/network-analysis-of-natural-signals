45ab5c52839a23f9518cefe7215a1332b67dbdad
Pankaj Singh <ps.jinx@gmail.com>
2012-04-03 16:11:02 +0530

    getting meaningful results I guess

--- main.m
+++ main.m
@@ -4,15 +4,16 @@ close all;
 
 [y, Fs, nbits, readinfo] =wavread('canadas.wav');
 
-[S, F, T, P] = spectrogram(y, hann(1024), 512, 1024, Fs);
+[S, F, T, P] = spectrogram(y, 256, 200, 256, Fs);
 
 [M, N] = size(P);
 
 Q = M/N;
 
-C_orig = corr(P);
-[V_orig, D_orig] = eig(C_orig);
-
+%C_orig = corr(P);
+%size(C_orig);
+% [V_orig, D_orig] = eig(C_orig);
+% 
 P_random = randomize(P);
 
 C_random = corr(P_random);
@@ -28,10 +29,13 @@ filtered_eigenvectors = [];
 [filtered_eigenvalues] = [filtered_eigenvalues; zeros(1,N)];
 [filtered_eigenvectors] = [filtered_eigenvectors; zeros(1,N)];
 
+Z =  zeros(1,N);
+
 for i = 2:N
+    i
     if D_random(i,i) < lambda_max && D_random(i,i) > lambda_min
-        [filtered_eigenvalues] = [filtered_eigenvalues; zeros(1,N)];
-        [filtered_eigenvectors] = [filtered_eigenvectors; zeros(1,N)];
+        [filtered_eigenvalues] = [filtered_eigenvalues; Z];
+        [filtered_eigenvectors] = [filtered_eigenvectors; Z];
     else
         [filtered_eigenvalues] = [filtered_eigenvalues; D_random(i, :)];
         [filtered_eigenvectors] = [filtered_eigenvectors; V_random(i, :)];
@@ -39,16 +43,36 @@ for i = 2:N
     end
 end
 
-spectrogram(y, hanning(1024), 512, 1024, Fs);
-C_orig = corr(P');
-%figure, imagesc(C_orig), colorbar;
-min(C_orig);
-min(min(C_orig));
-max(max(C_orig));
+save('filtered_eigenvalues.mat', 'filtered_eigenvalues');
+save('filtered_eigenvectors.mat', 'filtered_eigenvectors');
 
-AA=0.5+0.5*sign(C_orig);
+% c = [];
+% for i=1:N;
+%     i
+%     [c] = [c;filtered_eigenvalues(i,i)*filtered_eigenvectors(i, :)];
+% end
+%save('correlation_matrix_filtered_new.mat', 'c')
 
-%figure, imagesc(AA);
+% min(C_orig);
+% min(min(C_orig));
+% max(max(C_orig));
+% AA=0.5+0.5*sign(C_orig);
+
+AA = zeros(N);
+C_new = filtered_eigenvectors*filtered_eigenvalues/filtered_eigenvectors;
+save('C_new.mat', 'C_new');
+
+
+for i=1:N
+    i
+    for j=1:N
+        if C_new(i,j) > 0.02
+            AA(i,j) = 1;
+        end
+    end
+end
+
+% figure, imagesc(AA);
 
 
 doc = com.mathworks.xml.XMLUtils.createDocument('gexf');
@@ -81,6 +105,7 @@ for i=1:M
     node.setAttribute('id', sprintf('%i', i-1));
     node.setAttribute('label', sprintf('%f Hz ', F(i)));
     nodes.appendChild(node);
+    i
     for j=1:i
         if AA(i,j) > 0
             edge = doc.createElement('edge');
